cb58a33a61e1075a2c0b50460cf0df0363e8144a
FirdavsNurmatov <nurmatovfirdavs96@gmail.com>
2025-09-22 15:56:23 +0500

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import RoleChecker from "./components/RoleChecker";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route
+        path="/dashboard"
+        element={
+          <RoleChecker>
+            <Dashboard />
+          </RoleChecker>
+        }
+      />
     </Routes>
   );
 }
---
+++ src/components/RoleChecker.jsx
@@ -0,0 +1,13 @@
+import { Navigate } from "react-router-dom";
+import { useAuthStore } from "../store/useAuthStore";
+
+const RoleChecker = ({ children }) => {
+  const { user } = useAuthStore((store) => store);
+
+  if (!user.role) return <Navigate to="/" />;
+  if (!["ADMIN", "OPERATOR"].includes(user.role)) return <Navigate to="/" />;
+
+  return <>{children}</>;
+};
+
+export default RoleChecker;
